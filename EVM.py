cfee0bdfd58b3bb97471a3201f3553412c3220ba
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-12-11 13:05:58 +0100

    Fixed Language server suggestions

--- EVM.py
+++ EVM.py
@@ -4,14 +4,15 @@ def parse_storage(raw_storage: str) -> dict:
         return {}
     # TODO implement
 
+
 def parse_world_state(raw_state: str) -> dict:
     contracts = raw_state.split('x')
     world_state = {}
-    
+
     for contract in contracts:
         if contract == '':
             continue
-        
+
         contract_fields = contract.split(';')
         world_state.update({contract_fields[0]: {
             "nonce": int(contract_fields[1]),
@@ -20,31 +21,34 @@ def parse_world_state(raw_state: str) -> dict:
             "code": contract_fields[4],
             "code_hash": contract_fields[5],
         }})
-        
+
     return world_state
-        
+
 
 def revert(message: str):
     print(message)
     print("Call reverted!")
     exit(1)
-    
+
+
 def unimplemented(opcode: str):
     revert(f"Opcode: {opcode} is currnetly unimplemented!")
-    
+
+
 def is_account(address: str) -> bool:
-    return world_state.get(address) != None
+    return world_state.get(address) is not None
+
 
 def is_eoa(address: str) -> bool:
     if not is_account(address):
         return False
     account = get_account(address)
-    
+
     if account.get("code") != ' ':
         return False
-    
+
     return True
-    
+
 
 def get_account(address: str) -> dict:
     account = world_state.get(address)
@@ -52,16 +56,18 @@ def get_account(address: str) -> dict:
         revert("Account does not exist!")
     return account
 
+
 def get_eoa(address: str) -> dict:
     account = get_account(address)
-    
+
     if account.get("code") != ' ':
         revert("Account is not externally owned!")
-    
+
     return account
 
+
 if __name__ == "__main__":
-    
+
     # * Read input parameters
     # Parameter format: --param_name=param_value
     # Parameters:
@@ -70,28 +76,26 @@ if __name__ == "__main__":
     # value:    int         |   Ether amount in Wei
     # data:     str         |   Calldata string (optional)
     # gas:      int         |   Maximum amount of gas for the transaction
-    
+
     i_from = "4838b106fce9647bdf1e7877bf73ce8b0bad5f40"
     i_to = ""
     i_value = 0
     i_data = ""
     i_gas = 0
-    
+
     # TODO implement CLI input parameters
-        
-    
+
     # * Read most recent world state
     global world_state
-    
+
     with open("./WorldState.txt", 'r') as world_state_file:
         world_state = parse_world_state(world_state_file.readlines()[-1])
-        
+
     if not world_state:
         revert("Loading world state failed!")
-        
+
     print(world_state)
-    
-    
+
     # * Make transaction
     # Required parameters:
     # from:     address     |   Origin address
@@ -100,40 +104,30 @@ if __name__ == "__main__":
     # value:    int         |   Ether amount in Wei
     # data:     str         |   Calldata string
     # gas:      int         |   Maximum amount of gas for the transaction
-    
-    
+
     # Check sender validity (exists and is EOA)
     sender = get_eoa(i_from)
-    
+
     # Set from
     p_from = i_from
-    
+
     # Set transaction origin address
     tx_origin = i_from
-    
+
     # Set to
     p_to = i_to
-    
+
     # Set nonce
     p_nonce = sender.get("nonce")
-    
+
     # Check value validity
-    
+
     # Set calldata
-    
+
     # set gas
-    
+
     # Make transaction
-    
+
     # Increase nonce
-    
+
     # Save new world state
-    
-    
-        
-    
-    
-        
-    
-    
-    
